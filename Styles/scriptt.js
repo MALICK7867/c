924c35bbc2df289a1e0e5078f6d2bb45b6f51ed7
Mati Ur Rehman <104063208+MALICK7867@users.noreply.github.com>
2023-10-15 18:06:37 +0500

    Add files via upload

---
+++ Styles/scriptt.js
@@ -0,0 +1,16 @@
+const scrollBtn = document.querySelector('#scroll-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
