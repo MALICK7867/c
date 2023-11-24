b0867d388bde3470fce5d84e8b9dc32bb1d00356
Mati Ur Rehman <104063208+MALICK7867@users.noreply.github.com>
2023-11-24 15:15:39 +0500

    Add files via upload

---
+++ Styles/scriptt.js
@@ -0,0 +1,126 @@
+let x = 'none';
+let y = 'none';
+function op(operation){
+    x = operation;
+    chckpre();
+    if(x === 'a1'){
+        document.getElementById('article1').style.display = 'block';
+    }
+    else if(x === 'a2'){
+        document.getElementById('article2').style.display = 'block';
+    }
+    else if(x === 'a3'){
+        document.getElementById('article3').style.display = 'block';
+    }
+    else if(x === 'a4'){
+        document.getElementById('article4').style.display = 'block';
+    }
+    else if(x === 'a5'){
+        document.getElementById('article5').style.display = 'block';
+    }
+    else if(x === 'a6'){
+        document.getElementById('article6').style.display = 'block';
+    }
+    else if(x === 'a7'){
+        document.getElementById('article7').style.display = 'block';
+    }
+    else if(x === 'a8'){
+        document.getElementById('article8').style.display = 'block';
+    }
+    else if(x === 'a9'){
+        document.getElementById('article9').style.display = 'block';
+    }
+    else if(x === 'a10'){
+        document.getElementById('article10').style.display = 'block';
+    }
+    else if(x === 'a11'){
+        document.getElementById('article11').style.display = 'block';
+    }
+    else if(x === 'a12'){
+        document.getElementById('article12').style.display = 'block';
+    }
+    else if(x === 'a13'){
+        document.getElementById('article13').style.display = 'block';
+    }
+    else if(x === 'a14'){
+        document.getElementById('article14').style.display = 'block';
+    }
+    else if(x === 'a15'){
+        document.getElementById('article15').style.display = 'block';
+    }
+    else if(x === 'a16'){
+        document.getElementById('article16').style.display = 'block';
+    }
+    else if(x === 'a17'){
+        document.getElementById('article17').style.display = 'block';
+    }
+    else if(x === 'a18'){
+        document.getElementById('article18').style.display = 'block';
+    }
+    else if(x === 'a19'){
+        document.getElementById('article19').style.display = 'block';
+    }
+    y = x;
+}
+function chckpre(){
+    if (y === 'none'){
+        document.getElementById('article1').style.display = 'none';
+    }
+    else if ( y === 'a1' ){
+        document.getElementById('article1').style.display = 'none';
+    }
+    else if ( y === 'a2' ){
+        document.getElementById('article2').style.display = 'none';
+    }
+    else if ( y === 'a3' ){
+        document.getElementById('article3').style.display = 'none';
+    }
+    else if ( y === 'a4' ){
+        document.getElementById('article4').style.display = 'none';
+    }
+    else if ( y === 'a5' ){
+        document.getElementById('article5').style.display = 'none';
+    }
+    else if ( y === 'a6' ){
+        document.getElementById('article6').style.display = 'none';
+    }
+    else if ( y === 'a7' ){
+        document.getElementById('article7').style.display = 'none';
+    }
+    else if ( y === 'a8' ){
+        document.getElementById('article8').style.display = 'none';
+    }
+    else if ( y === 'a9' ){
+        document.getElementById('article9').style.display = 'none';
+    }
+    else if ( y === 'a10' ){
+        document.getElementById('article10').style.display = 'none';
+    }
+    else if ( y === 'a11' ){
+        document.getElementById('article11').style.display = 'none';
+    }
+    else if ( y === 'a12' ){
+        document.getElementById('article11').style.display = 'none';
+    }
+    else if ( y === 'a13' ){
+        document.getElementById('article13').style.display = 'none';
+    }
+    else if ( y === 'a14' ){
+        document.getElementById('article14').style.display = 'none';
+    }
+    else if ( y === 'a15' ){
+        document.getElementById('article15').style.display = 'none';
+    }
+    else if ( y === 'a16' ){
+        document.getElementById('article16').style.display = 'none';
+    }
+    else if ( y === 'a17' ){
+        document.getElementById('article17').style.display = 'none';
+    }
+    else if ( y === 'a18' ){
+        document.getElementById('article18').style.display = 'none';
+    }
+    else if ( y === 'a19' ){
+        document.getElementById('article19').style.display = 'none';
+    }
+}
